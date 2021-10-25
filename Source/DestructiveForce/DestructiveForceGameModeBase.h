68471a23b9d927d365fa2056c13a3efb9b01f390
Дмитрий Ветлужских <goodman0579@yandex.ru>
2021-10-25 21:14:50 +0300

    feat: add base content && Level_01 prototype

---
+++ Source/DestructiveForce/DestructiveForceGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DestructiveForceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESTRUCTIVEFORCE_API ADestructiveForceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
