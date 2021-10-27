e70cee6554d162687c8a140c92d1a18294914d46
Дмитрий Ветлужских <goodman0579@yandex.ru>
2021-10-27 21:23:35 +0300

    feat: add tank pawn with movement

---
+++ Source/DestructiveForce/Pawns/TankPawn.cpp
@@ -0,0 +1,79 @@
+﻿#include "TankPawn.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ATankPawn::ATankPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body Mesh"));;
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BodyMesh);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(BodyMesh);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATankPawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ATankPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	PerformMove(DeltaTime);
+	PerformRotate(DeltaTime);
+}
+
+void ATankPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ATankPawn::OnMoveForward);
+	PlayerInputComponent->BindAxis("TurnRight", this, &ATankPawn::OnTurnRight);
+}
+
+void ATankPawn::OnMoveForward(const float Value)
+{
+	ActiveForwardAxis = Value;
+}
+
+void ATankPawn::OnTurnRight(const float Value)
+{
+	ActiveRightAxis = Value;
+}
+
+void ATankPawn::PerformMove(const float DeltaTime)
+{
+	CurrentForwardRate = FMath::Lerp(CurrentForwardRate, ActiveForwardAxis, InterpolationSpeed);
+
+	const auto CurrentLocation = GetActorLocation();
+	const auto ForwardVector = GetActorForwardVector();
+	const auto MoveRate = MoveSpeed * CurrentForwardRate * ForwardVector * DeltaTime;
+	const auto NewActorLocation = CurrentLocation + MoveRate;
+
+	SetActorLocation(NewActorLocation, true);
+}
+
+void ATankPawn::PerformRotate(const float DeltaTime)
+{
+	CurrentRightRate = FMath::Lerp(CurrentRightRate, ActiveRightAxis, InterpolationSpeed);
+
+	const auto CurrentRotation = GetActorRotation();
+	const auto RotateRate = RotationSpeed * CurrentRightRate * DeltaTime;
+	const auto YawRotation = CurrentRotation.Yaw + RotateRate;
+	const auto NewActorRotation = FRotator(0.f, YawRotation, 0.f);
+
+	SetActorRotation(NewActorRotation);
+}
---
+++ Source/DestructiveForce/Pawns/TankPawn.h
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "TankPawn.generated.h"
+
+class UStaticMeshComponent;
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class DESTRUCTIVEFORCE_API ATankPawn : public APawn
+{
+	GENERATED_BODY()
+
+private:
+	float ActiveForwardAxis = 0.f;
+	float ActiveRightAxis = 0.f;
+	
+	float CurrentForwardRate = 0.f;
+	float CurrentRightRate = 0.f;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* BodyMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* Camera;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement|Speed")
+	float MoveSpeed = 100.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement|Speed")
+	float RotationSpeed = 100.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement|Speed")
+	float InterpolationSpeed = .1f;
+
+private:
+	void OnMoveForward(const float Value);
+	void OnTurnRight(const float Value);
+	
+	void PerformMove(float DeltaTime);
+	void PerformRotate(float DeltaTime);
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	ATankPawn();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
