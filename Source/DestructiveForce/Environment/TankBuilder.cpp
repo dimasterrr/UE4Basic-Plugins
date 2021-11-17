5c72edb8354772d4c9ccfae5051c2e05bcf20902
Дмитрий Ветлужских <goodman0579@yandex.ru>
2021-11-17 20:52:07 +0300

    feat(lesson07): add tank builder

---
+++ Source/DestructiveForce/Environment/TankBuilder.cpp
@@ -0,0 +1,39 @@
+﻿#include "TankBuilder.h"
+
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+#include "DestructiveForce/Controllers/TankAIController.h"
+#include "DestructiveForce/Pawns/EnemyTankPawn.h"
+
+ATankBuilder::ATankBuilder()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = BoxComponent = CreateDefaultSubobject<UBoxComponent>("Box Component");
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("Mesh Component");
+	MeshComponent->SetupAttachment(BoxComponent);
+
+	SpawnPointComponent = CreateDefaultSubobject<UArrowComponent>("Spawn Actor Component");
+	SpawnPointComponent->SetupAttachment(MeshComponent);
+}
+
+void ATankBuilder::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorldTimerManager().SetTimer(FSpawnTimerHandle, this, &ATankBuilder::OnSpawnActor, SpawnTime, true, 0.f);
+}
+
+void ATankBuilder::OnSpawnActor()
+{
+	if (!DefaultSpawnActor) return;
+
+	const auto SpawnedActor = GetWorld()->SpawnActorDeferred<AEnemyTankPawn>(DefaultSpawnActor,
+	                                                                         SpawnPointComponent->
+	                                                                         GetComponentTransform(),
+	                                                                         this, nullptr,
+	                                                                         ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn);
+	SpawnedActor->PatrolPoints = PatrolPoints;
+	SpawnedActor->FinishSpawning(SpawnPointComponent->GetComponentTransform());
+}
---
+++ Source/DestructiveForce/Environment/TankBuilder.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TankBuilder.generated.h"
+
+class ATargetPoint;
+class UArrowComponent;
+class UBoxComponent;
+class AEnemyTankPawn;
+
+UCLASS()
+class DESTRUCTIVEFORCE_API ATankBuilder : public AActor
+{
+	GENERATED_BODY()
+
+private:
+	FTimerHandle FSpawnTimerHandle;
+
+protected:
+	// Settings
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Settings")
+	float SpawnTime = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Settings")
+	TSubclassOf<AEnemyTankPawn> DefaultSpawnActor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Settings")
+	TArray<ATargetPoint*> PatrolPoints;
+
+	// Components
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components")
+	UBoxComponent* BoxComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components")
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components")
+	UArrowComponent* SpawnPointComponent;
+
+public:
+	ATankBuilder();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void OnSpawnActor();
+};
